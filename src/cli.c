d7bfaa6004a37341d9c194da66a48efcd210bbc7
voidtools <31149593+voidtools@users.noreply.github.com>
2025-05-30 19:22:36 +0930

    Add files via upload

--- src/cli.c
+++ src/cli.c
@@ -1,6 +1,6 @@
 
 //
-// Copyright (C) 2024 David Carpenter
+// Copyright (C) 2025 voidtools / David Carpenter
 // 
 // Permission is hereby granted, free of charge, 
 // to any person obtaining a copy of this software 
