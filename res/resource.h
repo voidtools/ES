5b55088a145d61bef996e3c8bb5a6286daebb65b
voidtools <31149593+voidtools@users.noreply.github.com>
2025-06-02 12:30:24 +0930

    Add files via upload

--- res/resource.h
+++ res/resource.h
@@ -3,3 +3,16 @@
 // Used by everything.rc
 //
 #define VS_VERSION_INFO                 1
+
+#define IDI_EVERYTHING                  101
+#define IDB_BITMAP1                     102
+#define IDI_ICON1                       103
+
+#define IDR_ICON_PNG                    104
+#define IDR_MAIN_CSS                    105
+#define IDR_DRIVE_PNG                   106
+#define IDR_EVERYTHING_PNG              107
+#define IDR_FILE_PNG                    108
+#define IDR_FOLDER_PNG                  109
+#define IDR_FAVICON_ICO                 110
+
